ba9abd42d251fe89ae904ce1fdfce81bfd21368e
Daniel Hoang <hoang.daniel2005@gmail.com>
2022-06-24 01:22:43 +0100

    finished header

--- main.js
+++ main.js
@@ -3,28 +3,30 @@ window.onload = function() {
     let tempWord = `${makeid(3)} ${makeid(5)}`;
     var randTitle = setInterval(() => {
         i++;
-        if (i > 90 ) {
-            tempWord = 'Dan Hoang';
+        if (i > 80 ) {
+            tempWord = '> Dan Hoang';
             clearInterval(randTitle);
-        } else if (i > 75) {
-            tempWord = `Dan Hoan${makeid(1)}`;
-        } else if (i > 70) {
-            tempWord = `Dan Hoa${makeid(2)}`;
         } else if (i > 65) {
-            tempWord = `Dan Ho${makeid(3)}`;
+            tempWord = `> Dan Hoan${makeid(1)}`;
         } else if (i > 60) {
-            tempWord = `Dan H${makeid(4)}`;
+            tempWord = `> Dan Hoa${makeid(2)}`;
+        } else if (i > 55) {
+            tempWord = `> Dan Ho${makeid(3)}`;
         } else if (i > 50) {
-            tempWord = `Dan ${makeid(5)}`;
-        } else if (i > 45) {
-            tempWord = `Da${makeid(1)} ${makeid(5)}`;
+            tempWord = `> Dan H${makeid(4)}`;
         } else if (i > 40) {
-            tempWord = `D${makeid(2)} ${makeid(5)}`;
+            tempWord = `> Dan ${makeid(5)}`;
+        } else if (i > 35) {
+            tempWord = `> Da${makeid(1)} ${makeid(5)}`;
+        } else if (i > 30) {
+            tempWord = `> D${makeid(2)} ${makeid(5)}`;
         } else {
-            tempWord = `${makeid(3)} ${makeid(5)}`;
+            tempWord = `> ${makeid(3)} ${makeid(5)}`;
         }
         document.getElementById("title").innerHTML = tempWord;
     }, 50);
+
+
 };
 
 function makeid(length) {
